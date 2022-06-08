c2a4661d5f433beb7fade80279c0e0044490bf89
Gerardo-Luna-Proo <87098518+Gerardo-Luna-Proo@users.noreply.github.com>
2022-06-08 17:59:35 -0500

    Add files via upload

---
+++ Reporductor Canciones/js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Harry Styles - As It Was",
+    artist: "Harry Styles",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Harry Styles - Golden",
+    artist: "Harry Styles",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Harry Styles - Adore You",
+    artist: "Harry Styles",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Harry Styles - Watermelon Sugar",
+    artist: "Harry Styles",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Kevin Kaarl - San Lucas",
+    artist: "Kevin Kaarl",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Kevin Kaarl - Colapso",
+    artist: "Kevin Kaarl",
+    img: "music-6",
+    src: "music-6"
+  },
+];
